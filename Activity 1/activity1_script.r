75a7842dd182c6bea3a6f32f06ed75953b1e34e0
Lucy Bonin '20 <lbonin@colgate.edu>
2020-01-24 12:12:34 -0500

    R code with calculations

--- Activity 1/activity1_script.r
+++ Activity 1/activity1_script.r
@@ -1,2 +1,12 @@
 # Activity 1
 print("hello R world")
+#subtraction
+200-136
+a<-1125
+#divide variable by 5
+a/5
+#create vector
+myvec<-c(5,25,225,625,a)
+#divide all numbers in the vector by 5
+myvec/5
+
