6e8c0099f5fe32c48c188d5834ab50a82934abe6
Lucy Bonin <lucybonin@Lucys-MacBook-Pro.local>
2020-04-01 00:39:12 -0400

    finishing comments

--- Activity 6/Activity6 Code.R
+++ Activity 6/Activity6 Code.R
@@ -59,8 +59,11 @@ str(NDVIraster[[1]])
 NDVIraster[[1]]@crs
 #HIGHER NDVI REPRESENTS MORE VEGETATION ON THE GROUND
 #plotting 2003
+#understanding to coordinate system
 plot(NDVIraster[[1]])
 
+#QUESTION 3
+#side by side plots of NDVI and g1966 to understand different coordinate systems
 par(mfrow=c(1,2))
 plot(NDVIraster[[1]])
 plot(g1966,axes=TRUE)
@@ -74,11 +77,13 @@ g2005p <- spTransform(g2005,NDVIraster[[1]]@crs)
 g2015p <- spTransform(g2015,NDVIraster[[1]]@crs)
 
 #QUESTION 4
+#plotting 2015 glacier data and the maximum NDVI to compare
 plot(NDVIraster[[13]],axes=FALSE)
 plot(g2015p,col=NA,add=TRUE,border="black")
 
 #QUESTION 5
-
+#calculate area for all polygons
+#add directly into data table for each shapefile
 g1966p@data$a1966m.sq <- area(g1966p)
 g1998p@data$a1998m.sq <- area(g1998p)
 g2005p@data$a2005m.sq <- area(g2005p)
@@ -87,6 +92,7 @@ gAllp1 <- join(g1966p@data,g1998p@data, by="GLACNAME", type="full")
 gAllp2 <- join(gAllp1,g2005p@data, by="GLACNAME", type="full")
 gAll <- join(gAllp2,g2015p@data, by="GLACNAME", type="full")
 
+#plot of the area of each glacier
 plot(c(1966,1998,2005,2015), 
      c(gAll$a1966m.sq[1],gAll$a1998m.sq[1], gAll$a2005m.sq[1],gAll$a2015m.sq[1]),
      type="b", 
@@ -103,7 +109,7 @@ for(i in 2:39){
   
 } 
 
-#calculating the percent change
+#calculating the percent change QUESTION 5
 percent_change<-list()
 for (i in 1:39){
   percent_change[[i]]<-((gAll$a2015m.sq[i]-gAll$a1966m.sq[i])/gAll$a1966m.sq[i])*100
@@ -112,13 +118,13 @@ percent_change
 g2015@data$percent_change<-percent_change
 spplot(g2015, "percent_change")
 
+#QUESTION 6
 percent_change<-as.numeric(percent_change)
 min(percent_change,na.rm=TRUE)
 #finding the name of the glacier
 g2015@data$GLACNAME[5]
 #I looked at the data to find the coordinates around Boulder Glacier
-#zoomed in accordingly to the coordinates
-
+#zoomed in accordingly to the coordinates to use for the plot
 plotRGB(rgbL, ext=c(273000.0,275000.0,5427000,5428000), stretch="lin", axes=TRUE,
         main="Percent Loss (84.72%) of Boulder Glacier over time")
 plot(g1966, col="palegreen2", border=NA, add=TRUE)
@@ -128,9 +134,9 @@ plot(g2015, col="tomato3", add=TRUE, border=NA)
 
 
 #QUESTION 7
-#extract NDVI values
 diffPoly <- gDifference(g1966p, g2015p)
 plot(diffPoly)
+#extract NDVI values
 NDVIdiff <- list()
 meanDiff <- numeric(0)
 #loop through all NDVI years
@@ -140,6 +146,11 @@ for(i in 1:length(ndviYear)){
   #calculate the mean of the NDVI values
   meanDiff[i] <- mean(NDVIdiff[[i]], na.rm=TRUE)
 }
+#plot of data
+plot(ndviYear, meanDiff, type="b",
+     xlab= "Year",
+     ylab="Average NDVI (unitless)",
+     pch=19)
 #buffer glaciers
 NDVIstack <- stack(NDVIraster)
 #set up lm function to apply to every cell
@@ -155,6 +166,9 @@ fun <- function(x) {
       lm(x ~ timeT)$coefficients[2] }}
 #apply the slope function to the rasters
 NDVIfit <- calc(NDVIstack,fun)
+#plot the change in NDVI
+plot(NDVIfit, axes=FALSE)
+#now the data has the slope for the change in NDVI per year
 
 #QUESTION 8
 glacier500m <- gBuffer(g1966p,#data to buffer
@@ -171,26 +185,33 @@ glacRaster <- rasterize(g1966p, NDVIraster[[1]], field=g1966p@data$GLACNAME, bac
 #subtract buffer from original glacier
 glacZones <- buffRaster - glacRaster
 plot(glacZones)
-
+#QUESTION 9
 meanChange <- zonal(NDVIfit, #NDVI function to summarize
                     glacZones,#raster with zones
                     "mean")#function to apply
 head(meanChange)
+#taking away the first row (zone 0)
 meanChange<-meanChange[-1,]
+#adding the second column of mean change (the first column just has the zone number)
+#to the g2015 data
 g2015@data$meanChange<-meanChange[,2]
+#plot of mean change in NDVI per year
 spplot(g2015, "meanChange")
 #looking at how it may change over 15 years
 mean15<-meanChange[,2]*15
 
 #Question 11
-#calculating
+#calculating the mean for NDVI over the park using NDVI stack
 NDVImean<-calc(NDVIstack,mean,na.rm=TRUE)
 avgZones<-zonal(NDVImean,glacZones,"mean")
 head(avgZones) #average vegetation for that zone over all the years
+#taking out zone 0
 avgZones<-avgZones[-1,]
+#putting it into the 2015 glacier polygon data
 g2015p@data$avgZones<-avgZones[,2]
-
+#used quantile to find the the thresholds to create a color scheme to show the average over time
 quantile(avgZones[,2])
+#for loop to match color to the values
 NDVIcol<-rep(NA, length(avgZones[,2]))
 for (i in 1:39){
   if (g2015p@data$avgZones[i]<0.25) {
@@ -203,11 +224,12 @@ for (i in 1:39){
     NDVIcol[i]<-"blue";
   }
 }
+#putting in the verctor of corresponding color values into the polygon data
 g2015p@data$NDVIcol<-NDVIcol
+#plot of the mean NDVI
 plot(NDVImean)
+#adding the polygons to the plot
 plot(g2015p,add=TRUE, col=paste(g2015p@data$NDVIcol),border=FALSE)
-legend("topright", c("avg NDVI <0.25","avg NDVI <0.33","avg NDVI <0.38","avg NDVI>0.38"), #legend items
-       col=c("lightblue","purple","red","blue"),#colors
-       bty="n")
+
 
 
